800971c36124e4d9188c1c9cc1d6a5b133fd8729
juliencombattelli <julien.combattelli@hotmail.com>
2015-05-15 21:06:28 +0200

    Add files headings.

--- 4DLibrary.cpp
+++ 4DLibrary.cpp
@@ -1,1 +1,7 @@
+/*
+* 4DLibrary.cpp
+* This file is a part of 4DLibrary
+* Copyright (c) 2015 juliencombattelli, SRTH
+*/
+
 #include "4DLibrary.hpp"
---
+++ 4DLibrary.hpp
@@ -0,0 +1,5 @@
+/*
+* 4DLibrary.hpp
+* This file is a part of 4DLibrary
+* Copyright (c) 2015 juliencombattelli, SRTH
+*/
