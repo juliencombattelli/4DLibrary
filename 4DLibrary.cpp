fe2598613a490e548f83522879a3df3b2cae574c
SRTH <guillaum324bis@hotmail.fr>
2015-05-15 23:55:14 +0200

    begining of txt function
    function list :
    
    unsigned char txt_move_cursor(unsigned int line, unsigned int column);
    
    unsigned char txt_put_char(unsigned char caractere);
    
    unsigned char txt_put_str(unsigned char* str);
    
    unsigned int txt_char_width(unsigned char car);
    
    unsigned int txt_char_height(unsigned char car);
    
    unsigned int txt_foreground_color(unsigned int color);
    
    unsigned int txt_background_color(unsigned int color);
    
    unsigned int txt_set_font(unsigned char font);

--- 4DLibrary.cpp
+++ 4DLibrary.cpp
@@ -40,3 +40,155 @@ void 4DLibrary::read_commande(unsigned char* cmd, unsigned char size)
 		cmd[i] = serial.getc();
 	}
 }
+
+/*
+* public function
+*/
+
+unsigned char 4DLibrary::txt_move_cursor(unsigned int line, unsigned int column)
+{
+	unsigned char cmd[6];
+	cmd[0] = 0xFF;
+	cmd[1] = 0xE9;
+	cmd[2] = (unsigned char)(line >> 8);
+	cmd[3] = (unsigned char)line;
+	cmd[4] = (unsigned char)(column >> 8);
+	cmd[5] = (unsigned char)column;
+	
+	write_commande(cmd, 6);
+	read_commande(cmd, 1);
+	if(cmd[0] = 0x06)
+		return 0;
+	else
+		return 1;
+}
+
+unsigned char 4DLibrary::txt_put_char(unsigned char caractere)
+{
+	unsigned char cmd[4];
+	cmd[0] = 0xFF;
+	cmd[1] = 0xFE;
+	cmd[2] = 0x00;
+	cmd[3] = caractere;
+	
+	write_commande(cmd, 4);
+	read_commande(cmd, 1);
+	if(cmd[0] = 0x06)
+		return 0;
+	else
+		return 1;
+}
+
+unsigned char 4DLibrary::txt_put_str(unsigned char* str)
+{
+	unsigned int size = strlen((unsigned char*)str);
+	unsigned char cmd[3];
+	cmd[0] = 0x00;
+	cmd[1] = 0x18;
+	cmd[2] = 0x00;
+	
+	write_commande(cmd, 2);
+	write_commande(str, size);
+	write_commande(&(cmd[2]), 1);
+	read_commande(cmd, 3);
+	if(cmd[0] = 0x06)
+	{
+		if( ((cmd[1]<<8) || cmd[2]) == size)
+			return 0;
+		else
+			return 2;
+	}
+	else
+		return 1;
+}
+
+unsigned int 4DLibrary::txt_char_width(unsigned char car)
+{
+	unsigned char cmd[3];
+	cmd[0] = 0x00;
+	cmd[1] = 0x1E;
+	cmd[2] = car;
+	
+	write_commande(cmd, 3);
+	read_commande(cmd, 3);
+	if(cmd[0] = 0x06)
+	{
+		return( (unsigned int)(cmd[1]<<8) || cmd[2]); // refert to datasheet
+	}
+	else
+		return 0; // impossible value -> error
+}
+
+unsigned int 4DLibrary::txt_char_height(unsigned char car)
+{
+	unsigned char cmd[3];
+	cmd[0] = 0x00;
+	cmd[1] = 0x1D;
+	cmd[2] = car;
+	
+	write_commande(cmd, 3);
+	read_commande(cmd, 3);
+	if(cmd[0] = 0x06)
+	{
+		return( (unsigned int)(cmd[1]<<8) || cmd[2]); // refert to datasheet
+	}
+	else
+		return 0; // impossible value -> error
+}
+
+unsigned int 4DLibrary::txt_foreground_color(unsigned int color)
+{
+	unsigned char cmd[4];
+	cmd[0] = 0xFF;
+	cmd[1] = 0xE7;
+	cmd[2] = (unsigned char)(color >> 8);
+	cmd[3] = (unsigned char)color;
+	
+	write_commande(cmd, 4);
+	read_commande(cmd, 3);
+	if(cmd[0] = 0x06)
+	{
+		return( (unsigned int)(cmd[1]<<8) || cmd[2]); // previous color
+	}
+	else
+		return 0xFFFF; // error
+}
+
+unsigned int 4DLibrary::txt_background_color(unsigned int color)
+{
+	unsigned char cmd[4];
+	cmd[0] = 0xFF;
+	cmd[1] = 0xE6;
+	cmd[2] = (unsigned char)(color >> 8);
+	cmd[3] = (unsigned char)color;
+	
+	write_commande(cmd, 4);
+	read_commande(cmd, 3);
+	if(cmd[0] = 0x06)
+	{
+		return( (unsigned int)(cmd[1]<<8) || cmd[2]); // previous color
+	}
+	else
+		return 0xFFFF; // error
+}
+
+unsigned int 4DLibrary::txt_set_font(unsigned char font)
+{
+	unsigned char cmd[4];
+	cmd[0] = 0xFF;
+	cmd[1] = 0xE5;
+	cmd[2] = 0x00;
+	if(font > 0 && font < 4)
+		cmd[3] = font - 1; // adaptation for font id
+	else
+		return( 0x00 ); // invalid font
+		
+	write_commande(cmd, 4);
+	read_commande(cmd, 3);
+	if(cmd[0] = 0x06)
+	{
+		return( (unsigned int)(cmd[1]<<8) || cmd[2]); // previous font
+	}
+	else
+		return 0xFFFF; // error
+}
--- 4DLibrary.hpp
+++ 4DLibrary.hpp
@@ -17,6 +17,25 @@ private :
 	void read_commande(unsigned char* cmd, unsigned char size);
 	
 public : 
+	
+	/*
+	* txt function
+	*/
+	unsigned char txt_move_cursor(unsigned int line, unsigned int column);
+	
+	unsigned char txt_put_char(unsigned char caractere);
+	
+	unsigned char txt_put_str(unsigned char* str);
+	
+	unsigned int txt_char_width(unsigned char car);
+	
+	unsigned int txt_char_height(unsigned char car);
+	
+	unsigned int txt_foreground_color(unsigned int color);
+	
+	unsigned int txt_background_color(unsigned int color);
+	
+	unsigned int txt_set_font(unsigned char font);
 
 };
 
